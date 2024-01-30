2bdc1ad42150fef814ac9882c8db4b87ccbf696c
StochasticMike12 <mfvarela@lcg.unam.mx>
2024-01-30 14:06:59 -0600

    Ejercicico grupal añadido.

---
+++ R/ejercicio_grupal.R
@@ -0,0 +1,14 @@
+# Ejercicio grupal | Clase: 30/01/2024
+#
+# - Elegimos los paquetes iNETgrate, scDesign3, scider y hapmapsnp6.
+# - Los paquetes scDesign3 y scider fueron desarrollados para trabajar con datos
+#   de single cell.
+# - En el caso de iNETgrate está destinado a construir redes de genes integrando
+#   datos de metilación y expresión.
+# - Por su parte hapmapsnp6 fue desarrollado solo para demostrar las
+#   herramientas de BioConductor.
+#
+# Discutimos sobre la documentación, disponibilidad en cada sisitema operativo
+# y si las preguntas sobre cada paquete han sido respondidas. En general todo
+# fue positivo excepto en el caso de hapmapsnp6, el cual carecía de
+# documentación y disponibilidad en windows y mac.
