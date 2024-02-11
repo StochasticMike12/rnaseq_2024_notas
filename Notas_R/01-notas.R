d9cb3429a65f24af923dbdf974b1f791fb39ef29
StochasticMike12 <mfvarela@lcg.unam.mx>
2024-02-11 02:51:59 -0600

    Cambios añadidos.

--- Notas_R/01-notas.R
+++ Notas_R/01-notas.R
@@ -1,1 +1,12 @@
-# Notas
+## Pasos para crear este RStudio project con este archivo de notas.
+usethis::create_project("~/rnaseq_2024_notas")
+usethis::use_r("01-notas.R")
+
+
+## Ejercicio 2
+usethis::use_r("02-visualizar-mtcars.R")
+
+
+## Ligar este proyecto a github
+usethis::use_git()
+usethis::use_github()
--- Notas_R/02-visualizar-mtcars.R
+++ Notas_R/02-visualizar-mtcars.R
@@ -24,3 +24,55 @@ dev.off()
 ## Para reproducir mi código
 options(width = 120)
 sessioninfo::session_info()
+
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.3.2 (2023-10-31 ucrt)
+# os       Windows 11 x64 (build 22631)
+# system   x86_64, mingw32
+# ui       RStudio
+# language (EN)
+# collate  Spanish_Mexico.utf8
+# ctype    Spanish_Mexico.utf8
+# tz       America/Mexico_City
+# date     2024-02-11
+# rstudio  2023.12.1+402 Ocean Storm (desktop)
+# pandoc   NA
+#
+# ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
+# package     * version date (UTC) lib source
+# cli           3.6.2   2023-12-11 [1] CRAN (R 4.3.2)
+# colorspace    2.1-0   2023-01-23 [1] CRAN (R 4.3.2)
+# dplyr         1.1.4   2023-11-17 [1] CRAN (R 4.3.2)
+# fansi         1.0.6   2023-12-08 [1] CRAN (R 4.3.2)
+# farver        2.1.1   2022-07-06 [1] CRAN (R 4.3.2)
+# fs            1.6.3   2023-07-20 [1] CRAN (R 4.3.2)
+# generics      0.1.3   2022-07-05 [1] CRAN (R 4.3.2)
+# ggplot2     * 3.4.4   2023-10-12 [1] CRAN (R 4.3.2)
+# glue          1.7.0   2024-01-09 [1] CRAN (R 4.3.2)
+# gtable        0.3.4   2023-08-21 [1] CRAN (R 4.3.2)
+# here        * 1.0.1   2020-12-13 [1] CRAN (R 4.3.2)
+# labeling      0.4.3   2023-08-29 [1] CRAN (R 4.3.1)
+# lifecycle     1.0.4   2023-11-07 [1] CRAN (R 4.3.2)
+# magrittr      2.0.3   2022-03-30 [1] CRAN (R 4.3.2)
+# munsell       0.5.0   2018-06-12 [1] CRAN (R 4.3.2)
+# pillar        1.9.0   2023-03-22 [1] CRAN (R 4.3.2)
+# pkgconfig     2.0.3   2019-09-22 [1] CRAN (R 4.3.2)
+# purrr         1.0.2   2023-08-10 [1] CRAN (R 4.3.2)
+# R6            2.5.1   2021-08-19 [1] CRAN (R 4.3.2)
+# rlang         1.1.3   2024-01-10 [1] CRAN (R 4.3.2)
+# rprojroot     2.0.4   2023-11-05 [1] CRAN (R 4.3.2)
+# rstudioapi    0.15.0  2023-07-07 [1] CRAN (R 4.3.2)
+# scales        1.3.0   2023-11-28 [1] CRAN (R 4.3.2)
+# sessioninfo * 1.2.2   2021-12-06 [1] CRAN (R 4.3.2)
+# tibble        3.2.1   2023-03-20 [1] CRAN (R 4.3.2)
+# tidyselect    1.2.0   2022-10-10 [1] CRAN (R 4.3.2)
+# usethis       2.2.2   2023-07-06 [1] CRAN (R 4.3.2)
+# utf8          1.2.4   2023-10-22 [1] CRAN (R 4.3.2)
+# vctrs         0.6.5   2023-12-01 [1] CRAN (R 4.3.2)
+# withr         3.0.0   2024-01-16 [1] CRAN (R 4.3.2)
+#
+# [1] C:/Users/nahua/AppData/Local/R/win-library/4.3
+# [2] C:/Program Files/R/R-4.3.2/library
+#
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
